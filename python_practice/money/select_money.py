c2770992c6447140e6b48fd0e3d0a17480eec0d5
cookie <xx521124@163.com>
2020-12-22 10:36:11 +0800

    提交money

---
+++ python_practice/money/select_money.py
@@ -0,0 +1,7 @@
+# 查询工资方法
+import money
+
+def select_money():
+    print(f"当前工资为{money.saved_money}")
+
+# select_money()
---
+++ python_practice/money/send_money.py
@@ -0,0 +1,6 @@
+# 发工资方法
+import money
+
+def send_money():
+    print("发工资啦！")
+    money.saved_money = money.saved_money + 1000
